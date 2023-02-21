1c1eba5f4a63365836668c73e757ae1aed28211b
Joshua3212 <joshua-koeck@gmx.de>
2023-02-21 21:12:19 +0100

    wip tickets generator

---
+++ scripts/tickets/ticket_generator.py
@@ -0,0 +1,30 @@
+"""
+Core idea: Generate x cards with ids from 1 to x and apply funky styling.
+The cards will be stored in ./cards (hopefully :)
+"""
+import random
+import time
+
+import jinja2
+import pdfkit
+
+tickets_to_gen = 3
+bg_gradients = [
+    "linear-gradient(red, blue)",
+    "linear-gradient(red, green)",
+    "linear-gradient(yellow, blue)",
+]
+
+for i in range(1, tickets_to_gen + 1):
+    context = {
+        "id": i,
+        "created": int(time.time()),
+        "bg_gradient": bg_gradients[random.randint(0, 2)]
+    }
+
+    fs = jinja2.FileSystemLoader("./")
+    jinja2.Environment(loader=fs)
+
+    template = jinja2.Environment(loader=fs).get_template("template.html")
+    output_html = template.render(context)
+    output_pdf = pdfkit.from_string(output_html, f"./tickets/{i}.pdf")

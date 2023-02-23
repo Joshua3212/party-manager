fbfb85a11ceff7ad8f5055f5120ad6e184d128b8
Joshua3212 <joshua-koeck@gmx.de>
2023-02-23 18:11:01 +0100

    wip tickets generator

--- scripts/tickets/ticket_generator.py
+++ scripts/tickets/ticket_generator.py
@@ -4,9 +4,11 @@ The cards will be stored in ./cards (hopefully :)
 """
 import random
 import time
+import urllib.parse
 
 import html2image
 import jinja2
+import qrcode.image.svg
 
 tickets_to_gen = 3
 bg_gradients = [
@@ -16,7 +18,10 @@ bg_gradients = [
 ]
 
 for i in range(1, tickets_to_gen + 1):
+    qr_code = qrcode.make(i, image_factory=qrcode.image.svg.SvgImage)
+    as_svg = qr_code.to_string(encoding="unicode")
     context = {
+        "qr": urllib.parse.quote(as_svg),
         "id": i,
         "created": int(time.time()),
         "bg_gradient": bg_gradients[random.randint(0, 2)]
@@ -27,4 +32,5 @@ for i in range(1, tickets_to_gen + 1):
 
     template = jinja2.Environment(loader=fs).get_template("template.html")
     output_html = template.render(context)
+    # print(output_html)
     html2image.Html2Image(size=(1100, 900)).screenshot(html_str=output_html, save_as=f"{i}.png")
